d390f40342b7b558401f1e095f59c19803124de4
DaffaYuza <muhammaddaffayuza@gmail.com>
2023-07-20 20:14:34 +0700

    menambahkan search

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-// toggel class active
+// toggel class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 // ketika humburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
+
+// toggel class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+}
 
 // klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector('#hamburger-menu');
