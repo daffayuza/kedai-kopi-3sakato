d0a87228191901d46acf2acb8460322f81b2b7fd
DaffaYuza <muhammaddaffayuza@gmail.com>
2023-11-14 16:07:22 +0700

    gassken

--- js/script.js
+++ js/script.js
@@ -14,13 +14,53 @@ document.querySelector('#search-button').onclick = (e) => {
   searchForm.classList.toggle('active');
   searchBox.focus();
   e.preventDefault();
-}
+};
+
+// togel class active untuk Shopping Cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+// ketika Shopping Cart di klik
+document.querySelector('#shopping-cart-button').onclick = () => {
+  shoppingCart.classList.toggle('active');
+};
 
 // klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+const hb = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const scb = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hb.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+  if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik diluar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
