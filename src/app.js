c0e70f33462427ea89af3e13873e903ba9ad34a1
DaffaYuza <muhammaddaffayuza@gmail.com>
2023-12-09 20:06:50 +0700

    update

--- src/app.js
+++ src/app.js
@@ -68,6 +68,48 @@ document.addEventListener('alpine:init', () => {
   });
 });
 
+// Form Validation
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm')
+
+form.addEventListener('keyup', function(){
+  for( let i = 0; i < form.elements.length; i++){
+    if(form.elements[i].value.length !== 0){
+      checkoutButton.classList.remove('disabled');
+      checkoutButton.classList.add('disabled');
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove('disabled');
+});
+
+// kirim data ketika tombol checkout diklik
+checkoutButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open('http://wa.me/6281365116963?text=' + encodeURIComponent(message));
+});
+
+// format pesan Whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama : ${obj.name}
+  Email : ${obj.email}
+  NO HP : ${obj.phone} \n
+Data Pesanan 
+  ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} X ${rupiah(item.total)}) \n`)}
+  TOTAL : ${rupiah(obj.total)}
+  Terima Kasih.`;
+};
+
+
 // Konversi ke Rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat('id-ID', {
